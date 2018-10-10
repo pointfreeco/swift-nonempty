64504fe6c3825b1840c6b268ca990b6b373b8df4
Stephen Celis <stephen@stephencelis.com>
2018-10-10 19:26:54 -0400

    Auto-derive Derivable Conformances

--- Sources/NonEmpty/NonEmpty+Equatable.swift
+++
@@ -1,5 +0,0 @@
-extension NonEmpty: Equatable where C: Equatable, C.Element: Equatable {
-  public static func == (lhs: NonEmpty, rhs: NonEmpty) -> Bool {
-    return lhs.head == rhs.head && lhs.tail == rhs.tail
-  }
-}
