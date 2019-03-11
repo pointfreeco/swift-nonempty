0c5b88f8c3a343696c2edc6d436c6867d1882214
Stephen Celis <stephen@stephencelis.com>
2019-03-11 16:17:45 -0400

    Bump

--- Sources/NonEmpty/NonEmpty+Dictionary.swift
+++ Sources/NonEmpty/NonEmpty+Dictionary.swift
@@ -106,8 +106,8 @@ extension NonEmpty where C: _DictionaryProtocol {
 
 extension NonEmpty where C: _DictionaryProtocol, C.Value: Equatable {
   public static func == (lhs: NonEmpty, rhs: NonEmpty) -> Bool {
-    return Dictionary<C.Key, C.Value>(uniqueKeysWithValues: Array<Element>(lhs))
-      == Dictionary<C.Key, C.Value>(uniqueKeysWithValues: Array<Element>(rhs))
+    return Dictionary(uniqueKeysWithValues: Array(lhs))
+      == Dictionary(uniqueKeysWithValues: Array(rhs))
   }
 }
 
