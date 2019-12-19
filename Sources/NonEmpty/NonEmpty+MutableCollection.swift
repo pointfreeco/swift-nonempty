514d845209e253676ce2bc19a135b378976ecca7
Brandon Williams <mbw234@gmail.com>
2019-12-19 18:28:08 -0500

    try

--- Sources/NonEmpty/NonEmpty+MutableCollection.swift
+++ Sources/NonEmpty/NonEmpty+MutableCollection.swift
@@ -1,21 +1,21 @@
-//extension NonEmpty: MutableCollection where C: MutableCollection {
-//  public subscript(position: Index) -> Element {
-//    get {
-//      switch position {
-//      case .head:
-//        return self.head
-//      case let .tail(index):
-//        return self.tail[index]
-//      }
-//    }
-//    set {
-//      switch position {
-//      case .head:
-//        self.head = newValue
-//      case let .tail(index):
-//        self.tail[index] = newValue
-//      }
-//    }
+extension NonEmpty: MutableCollection where C: MutableCollection {
+  public subscript(position: Index) -> Element {
+    get {
+      switch position {
+      case .head:
+        return self.head
+      case let .tail(index):
+        return self.tail[index]
+      }
+    }
+    set {
+      switch position {
+      case .head:
+        self.head = newValue
+      case let .tail(index):
+        self.tail[index] = newValue
+      }
+    }
 //    _modify {
 //      switch position {
 //      case .head:
@@ -24,19 +24,19 @@
 //        yield &self.tail[index]
 //      }
 //    }
-//  }
-//}
-//
-//extension NonEmpty where C: MutableCollection, C.Index == Int {
-//  public subscript(position: Int) -> Element {
-//    get {
-//      return self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)]
-//    }
-//    set {
-//      self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)] = newValue
-//    }
+  }
+}
+
+extension NonEmpty where C: MutableCollection, C.Index == Int {
+  public subscript(position: Int) -> Element {
+    get {
+      return self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)]
+    }
+    set {
+      self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)] = newValue
+    }
 //    _modify {
 //      yield &self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)]
 //    }
-//  }
-//}
+  }
+}
