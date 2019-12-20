d9a9824fe683d65de47791059f27e6483977ce20
Brandon Williams <mbw234@gmail.com>
2019-12-19 19:02:57 -0500

    wip

--- Sources/NonEmpty/NonEmpty+MutableCollection.swift
+++ Sources/NonEmpty/NonEmpty+MutableCollection.swift
@@ -16,14 +16,14 @@ extension NonEmpty: MutableCollection where C: MutableCollection {
         self.tail[index] = newValue
       }
     }
-//    _modify {
-//      switch position {
-//      case .head:
-//        yield &self.head
-//      case let .tail(index):
-//        yield &self.tail[index]
-//      }
-//    }
+    _modify {
+      switch position {
+      case .head:
+        yield &self.head
+      case let .tail(index):
+        yield &self.tail[index]
+      }
+    }
   }
 }
 
@@ -35,8 +35,8 @@ extension NonEmpty where C: MutableCollection, C.Index == Int {
     set {
       self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)] = newValue
     }
-//    _modify {
-//      yield &self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)]
-//    }
+    _modify {
+      yield &self[position == 0 ? .head : .tail(self.tail.startIndex + position - 1)]
+    }
   }
 }
