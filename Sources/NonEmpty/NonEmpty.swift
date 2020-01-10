a9fff7bc826936a0525b4d2e5d87f783b5124e26
Brandon Williams <mbw234@gmail.com>
2020-01-09 19:01:51 -0500

    wip

--- Sources/NonEmpty/NonEmpty.swift
+++ Sources/NonEmpty/NonEmpty.swift
@@ -16,10 +16,10 @@ extension NonEmpty: CustomStringConvertible {
   }
 }
 
-extension NonEmpty: Equatable where C: Equatable, C.Element: Equatable {}
-
-extension NonEmpty: Hashable where C: Hashable, C.Element: Hashable {}
-
-extension NonEmpty: Decodable where C: Decodable, C.Element: Decodable {}
-
-extension NonEmpty: Encodable where C: Encodable, C.Element: Encodable {}
+//extension NonEmpty: Equatable where C: Equatable, C.Element: Equatable {}
+//
+//extension NonEmpty: Hashable where C: Hashable, C.Element: Hashable {}
+//
+//extension NonEmpty: Decodable where C: Decodable, C.Element: Decodable {}
+//
+//extension NonEmpty: Encodable where C: Encodable, C.Element: Encodable {}
