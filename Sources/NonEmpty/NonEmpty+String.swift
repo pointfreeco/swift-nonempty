0cdbf8d97da361e1c4ff09b82565ce357751db49
Stephen Celis <stephen@stephencelis.com>
2020-02-24 18:20:50 -0500

    Fix?

--- Sources/NonEmpty/NonEmpty+String.swift
+++ Sources/NonEmpty/NonEmpty+String.swift
@@ -45,6 +45,7 @@ extension NonEmpty: LosslessStringConvertible where C: LosslessStringConvertible
 
 extension NonEmpty: ExpressibleByStringInterpolation where C: ExpressibleByStringInterpolation, C.StringLiteralType == DefaultStringInterpolation.StringLiteralType {}
 
+#if swift(>=5.2)
 extension NonEmpty: StringProtocol where C: StringProtocol {
   public typealias UTF8View = C.UTF8View
   public typealias UTF16View = C.UTF16View
@@ -85,3 +86,45 @@ extension NonEmpty: StringProtocol where C: StringProtocol {
     try self.withCString(encodedAs: targetEncoding, body)
   }
 }
+#else
+extension NonEmpty where C: StringProtocol {
+  public typealias UTF8View = C.UTF8View
+  public typealias UTF16View = C.UTF16View
+  public typealias UnicodeScalarView = C.UnicodeScalarView
+
+  public var utf8: UTF8View { self.rawValue.utf8 }
+  public var utf16: UTF16View { self.rawValue.utf16 }
+  public var unicodeScalars: UnicodeScalarView { self.rawValue.unicodeScalars }
+
+  public func lowercased() -> String { self.rawValue.lowercased() }
+  public func uppercased() -> String { self.rawValue.uppercased() }
+
+  public init<CodeUnits, Encoding>(
+    decoding codeUnits: CodeUnits, as sourceEncoding: Encoding.Type
+  ) where CodeUnits: Collection, Encoding: _UnicodeEncoding, CodeUnits.Element == Encoding.CodeUnit {
+    self.init(rawValue: C(decoding: codeUnits, as: sourceEncoding))!
+  }
+
+  public init(cString nullTerminatedUTF8: UnsafePointer<CChar>) {
+    self.init(rawValue: C(cString: nullTerminatedUTF8))!
+  }
+
+  public init<Encoding>(
+    decodingCString nullTerminatedCodeUnits: UnsafePointer<Encoding.CodeUnit>,
+    as sourceEncoding: Encoding.Type
+  ) where Encoding: _UnicodeEncoding {
+    self.init(rawValue: C(decodingCString: nullTerminatedCodeUnits, as: sourceEncoding))!
+  }
+
+  public func withCString<Result>(_ body: (UnsafePointer<CChar>) throws -> Result) rethrows -> Result {
+    try self.rawValue.withCString(body)
+  }
+
+  public func withCString<Result, Encoding>(
+    encodedAs targetEncoding: Encoding.Type,
+    _ body: (UnsafePointer<Encoding.CodeUnit>) throws -> Result
+  ) rethrows -> Result where Encoding: _UnicodeEncoding {
+    try self.withCString(encodedAs: targetEncoding, body)
+  }
+}
+#endif
