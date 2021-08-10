75b2454e6d54d1d48f9ef9ccb0c75bec84b75693
Stephen Celis <stephen@stephencelis.com>
2021-08-10 12:24:07 -0400

    Fix compiler crash

--- Sources/NonEmpty/NonEmpty+String.swift
+++ Sources/NonEmpty/NonEmpty+String.swift
@@ -59,112 +59,50 @@ where
   Collection.StringLiteralType == DefaultStringInterpolation.StringLiteralType
 {}
 
-#if swift(>=5.2)
-  extension NonEmpty: StringProtocol where Collection: StringProtocol {
-    public typealias UTF8View = Collection.UTF8View
-    public typealias UTF16View = Collection.UTF16View
-    public typealias UnicodeScalarView = Collection.UnicodeScalarView
+extension NonEmpty where Collection: StringProtocol {
+  public typealias UTF8View = Collection.UTF8View
+  public typealias UTF16View = Collection.UTF16View
+  public typealias UnicodeScalarView = Collection.UnicodeScalarView
 
-    public var utf8: UTF8View { self.rawValue.utf8 }
-    public var utf16: UTF16View { self.rawValue.utf16 }
-    public var unicodeScalars: UnicodeScalarView { self.rawValue.unicodeScalars }
+  public var utf8: UTF8View { self.rawValue.utf8 }
+  public var utf16: UTF16View { self.rawValue.utf16 }
+  public var unicodeScalars: UnicodeScalarView { self.rawValue.unicodeScalars }
 
-    @_disfavoredOverload
-    public func lowercased() -> String { self.rawValue.lowercased() }
-
-    @_disfavoredOverload
-    public func uppercased() -> String { self.rawValue.uppercased() }
-
-    public init<C, Encoding>(
-      decoding codeUnits: C, as sourceEncoding: Encoding.Type
-    ) where C: Swift.Collection, Encoding: _UnicodeEncoding, C.Element == Encoding.CodeUnit {
-      self.init(rawValue: Collection(decoding: codeUnits, as: sourceEncoding))!
-    }
-
-    public init(cString nullTerminatedUTF8: UnsafePointer<CChar>) {
-      self.init(rawValue: Collection(cString: nullTerminatedUTF8))!
-    }
-
-    public init<Encoding>(
-      decodingCString nullTerminatedCodeUnits: UnsafePointer<Encoding.CodeUnit>,
-      as sourceEncoding: Encoding.Type
-    ) where Encoding: _UnicodeEncoding {
-      self.init(rawValue: Collection(decodingCString: nullTerminatedCodeUnits, as: sourceEncoding))!
-    }
-
-    public func withCString<Result>(_ body: (UnsafePointer<CChar>) throws -> Result) rethrows
-      -> Result
-    {
-      try self.rawValue.withCString(body)
-    }
-
-    public func withCString<Result, Encoding>(
-      encodedAs targetEncoding: Encoding.Type,
-      _ body: (UnsafePointer<Encoding.CodeUnit>) throws -> Result
-    ) rethrows -> Result where Encoding: _UnicodeEncoding {
-      try self.rawValue.withCString(encodedAs: targetEncoding, body)
-    }
-
-    public func lowercased() -> NonEmptyString {
-      NonEmptyString(self.rawValue.lowercased())!
-    }
-
-    public func uppercased() -> NonEmptyString {
-      NonEmptyString(self.rawValue.uppercased())!
-    }
+  public init<C, Encoding>(
+    decoding codeUnits: C, as sourceEncoding: Encoding.Type
+  ) where C: Swift.Collection, Encoding: _UnicodeEncoding, C.Element == Encoding.CodeUnit {
+    self.init(rawValue: Collection(decoding: codeUnits, as: sourceEncoding))!
   }
-#else
-  extension NonEmpty where Collection: StringProtocol {
-    public typealias UTF8View = Collection.UTF8View
-    public typealias UTF16View = Collection.UTF16View
-    public typealias UnicodeScalarView = Collection.UnicodeScalarView
 
-    public var utf8: UTF8View { self.rawValue.utf8 }
-    public var utf16: UTF16View { self.rawValue.utf16 }
-    public var unicodeScalars: UnicodeScalarView { self.rawValue.unicodeScalars }
-
-    @_disfavoredOverload
-    public func lowercased() -> String { self.rawValue.lowercased() }
-
-    @_disfavoredOverload
-    public func uppercased() -> String { self.rawValue.uppercased() }
-
-    public init<C, Encoding>(
-      decoding codeUnits: C, as sourceEncoding: Encoding.Type
-    ) where C: Swift.Collection, Encoding: _UnicodeEncoding, C.Element == Encoding.CodeUnit {
-      self.init(rawValue: Collection(decoding: codeUnits, as: sourceEncoding))!
-    }
-
-    public init(cString nullTerminatedUTF8: UnsafePointer<CChar>) {
-      self.init(rawValue: Collection(cString: nullTerminatedUTF8))!
-    }
-
-    public init<Encoding>(
-      decodingCString nullTerminatedCodeUnits: UnsafePointer<Encoding.CodeUnit>,
-      as sourceEncoding: Encoding.Type
-    ) where Encoding: _UnicodeEncoding {
-      self.init(rawValue: Collection(decodingCString: nullTerminatedCodeUnits, as: sourceEncoding))!
-    }
-
-    public func withCString<Result>(_ body: (UnsafePointer<CChar>) throws -> Result) rethrows
-      -> Result
-    {
-      try self.rawValue.withCString(body)
-    }
-
-    public func withCString<Result, Encoding>(
-      encodedAs targetEncoding: Encoding.Type,
-      _ body: (UnsafePointer<Encoding.CodeUnit>) throws -> Result
-    ) rethrows -> Result where Encoding: _UnicodeEncoding {
-      try self.withCString(encodedAs: targetEncoding, body)
-    }
-
-    public func lowercased() -> NonEmptyString {
-      NonEmptyString(self.rawValue.lowercased())!
-    }
-
-    public func uppercased() -> NonEmptyString {
-      NonEmptyString(self.rawValue.uppercased())!
-    }
+  public init(cString nullTerminatedUTF8: UnsafePointer<CChar>) {
+    self.init(rawValue: Collection(cString: nullTerminatedUTF8))!
   }
-#endif
+
+  public init<Encoding>(
+    decodingCString nullTerminatedCodeUnits: UnsafePointer<Encoding.CodeUnit>,
+    as sourceEncoding: Encoding.Type
+  ) where Encoding: _UnicodeEncoding {
+    self.init(rawValue: Collection(decodingCString: nullTerminatedCodeUnits, as: sourceEncoding))!
+  }
+
+  public func withCString<Result>(_ body: (UnsafePointer<CChar>) throws -> Result) rethrows
+    -> Result
+  {
+    try self.rawValue.withCString(body)
+  }
+
+  public func withCString<Result, Encoding>(
+    encodedAs targetEncoding: Encoding.Type,
+    _ body: (UnsafePointer<Encoding.CodeUnit>) throws -> Result
+  ) rethrows -> Result where Encoding: _UnicodeEncoding {
+    try self.rawValue.withCString(encodedAs: targetEncoding, body)
+  }
+
+  public func lowercased() -> NonEmptyString {
+    NonEmptyString(self.rawValue.lowercased())!
+  }
+
+  public func uppercased() -> NonEmptyString {
+    NonEmptyString(self.rawValue.uppercased())!
+  }
+}
