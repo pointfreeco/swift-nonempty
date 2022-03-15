d4696ec9b1ff96fdda2855a2d66563753423faf2
Stephen Celis <stephen.celis@gmail.com>
2022-03-15 17:44:56 -0400

    Update Sources/NonEmpty/NonEmpty+ExpressibleByDictionaryLiteral.swift

--- Sources/NonEmpty/NonEmpty+ExpressibleByDictionaryLiteral.swift
+++ Sources/NonEmpty/NonEmpty+ExpressibleByDictionaryLiteral.swift
@@ -1,6 +1,6 @@
 extension NonEmpty: ExpressibleByDictionaryLiteral where Collection: ExpressibleByDictionaryLiteral {
   public init(dictionaryLiteral elements: (Collection.Key, Collection.Value)...) {
-    assert(!elements.isEmpty, "Cannot initialize a NonEmpty dictionary from an empty literal!")
+    precondition(!elements.isEmpty, "Can't construct \(Self.self) from empty dictionary literal")
     let f = unsafeBitCast(
       Collection.init(dictionaryLiteral:),
       to: (([(Collection.Key, Collection.Value)]) -> Collection).self
