0787f25d7b8372c73483fb0695412d2856ec56cb
fwcd <fwcdmail@gmail.com>
2022-03-15 22:23:13 +0100

    Implement ExpressibleBy conformances directly in terms of them

--- Sources/NonEmpty/NonEmpty+ExpressibleByArrayLiteral.swift
+++ Sources/NonEmpty/NonEmpty+ExpressibleByArrayLiteral.swift
@@ -1,14 +1,10 @@
-extension NonEmpty: ExpressibleByArrayLiteral where Collection: _InitializableFromArray {
+extension NonEmpty: ExpressibleByArrayLiteral where Collection: ExpressibleByArrayLiteral {
   public init(arrayLiteral elements: Element...) {
     assert(!elements.isEmpty, "Cannot initialize a NonEmpty array from an empty literal!")
-    self.init(rawValue: Collection(elements))!
+    let f = unsafeBitCast(
+      Collection.init(arrayLiteral:),
+      to: (([Element]) -> Collection).self
+    )
+    self.init(rawValue: f(elements))!
   }
 }
-
-public protocol _InitializableFromArray {
-  associatedtype Element
-
-  init(_ array: [Element])
-}
-
-extension Array: _InitializableFromArray {}
--- Sources/NonEmpty/NonEmpty+ExpressibleByDictionaryLiteral.swift
+++ Sources/NonEmpty/NonEmpty+ExpressibleByDictionaryLiteral.swift
@@ -1,15 +1,10 @@
-extension NonEmpty: ExpressibleByDictionaryLiteral where Collection: _InitializableFromKeyValuePairs {
+extension NonEmpty: ExpressibleByDictionaryLiteral where Collection: ExpressibleByDictionaryLiteral {
   public init(dictionaryLiteral elements: (Collection.Key, Collection.Value)...) {
     assert(!elements.isEmpty, "Cannot initialize a NonEmpty dictionary from an empty literal!")
-    self.init(rawValue: Collection(uniqueKeysWithValues: elements))!
+    let f = unsafeBitCast(
+      Collection.init(dictionaryLiteral:),
+      to: (([(Collection.Key, Collection.Value)]) -> Collection).self
+    )
+    self.init(rawValue: f(elements))!
   }
 }
-
-public protocol _InitializableFromKeyValuePairs {
-  associatedtype Key
-  associatedtype Value
-
-  init(uniqueKeysWithValues: [(Key, Value)])
-}
-
-extension Dictionary: _InitializableFromKeyValuePairs {}
